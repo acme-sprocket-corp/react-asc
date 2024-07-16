2ea7597b0607cfcd265e2c046e808bdf88f2e263
Michael Bradvica <mjbradvica@gmail.com>
2024-07-15 20:04:33 -0400

    more config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const element = document.querySelector("#root") ?? new Element();
+
+// eslint-disable-next-line vitest/require-hook
+ReactDOM.createRoot(element).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
